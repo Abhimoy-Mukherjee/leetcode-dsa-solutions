0a44d98ed153c30204f123a0e9308fc0d4428799
Abhimoy Mukherjee <abhimoymukherjee@gmail.com>
2026-08-21 13:32:00 +0530

    Time: 2 ms (98.22%) | Memory: 47.1 MB (85.47%) - LeetSync

---
+++ 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.java
@@ -0,0 +1,72 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    Deque<TreeNode> asc = new ArrayDeque<>();
+    Deque<TreeNode> desc = new ArrayDeque<>();
+    public boolean findTarget(TreeNode root, int k) {
+        if(root==null)
+            return false;
+        TreeNode t=root;
+        while(t!=null)
+        {
+            asc.push(t);
+            t=t.left;
+        }
+        t=root;
+        while(t!=null)
+        {
+            desc.push(t);
+            t=t.right;
+        }
+        TreeNode i=getSmall();
+        TreeNode j=getBig();
+        while((i!=null && j!=null) && (i!=j) && (i.val<=j.val))
+        {
+            int sum=i.val+j.val;
+            if(sum==k)
+                return true;
+            if(sum>k)
+                j=getBig();
+            else
+                i=getSmall();
+        }
+        return false;
+    }
+    public TreeNode getSmall(){
+        if(asc.isEmpty())
+            return null;
+        TreeNode small=asc.pop();
+        TreeNode rightchild=small.right;
+        while(rightchild!=null)
+        {
+            asc.push(rightchild);
+            rightchild=rightchild.left;
+        }
+        return small;
+    }
+    public TreeNode getBig(){
+        if(desc.isEmpty())
+            return null;
+        TreeNode big=desc.pop();
+        TreeNode leftchild=big.left;
+        while(leftchild!=null)
+        {
+            desc.push(leftchild);
+            leftchild=leftchild.right;
+        }
+        return big;
+    }
+}
