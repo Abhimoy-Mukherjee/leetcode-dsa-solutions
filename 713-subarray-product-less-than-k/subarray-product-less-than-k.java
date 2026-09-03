4736aa10c759b93ab4709d6f93b2866b392deebe
Abhimoy Mukherjee <abhimoymukherjee@gmail.com>
2026-09-03 15:52:34 +0530

    Time: 2 ms (99.96%) | Memory: 49 MB (42.57%) - LeetSync

---
+++ 713-subarray-product-less-than-k/subarray-product-less-than-k.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int numSubarrayProductLessThanK(int[] nums, int k) {
+        if(k<=1)
+            return 0;
+        int left=0;
+        int pro=1;
+        int count=0;
+        for(int right=0;right<nums.length;right++)
+        {
+            pro*=nums[right];
+            while(pro>=k)
+                pro/=nums[left++];
+            count+=right-left+1;
+        }
+        return count;
+    }
+}
